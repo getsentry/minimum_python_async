b237b28af0f017d4014ddb19e3c534db8c85d1e2
Anton Pirker <anton.pirker@sentry.io>
2022-10-06 12:03:42 +0200

    Added lots of debug output

--- main.py
+++ main.py
@@ -38,16 +38,24 @@ class CounterMiddleware:
         path = scope.get("path")
 
         hub = sentry_sdk.Hub.current
+        print(f"~~~ 1 current span: {hub.scope.span}")        
         with hub.start_span(op="cache.save", description="in the request phase of CounterMiddleware") as middleware_span:
-            print('so something here with request data')
+            print(f"~~~~~ 1 started span: {middleware_span}")
+            print(f"~~~~~ 1 hub: {hub}")
+            print(f"~~~~~ 1 scope: {hub.scope}")
+            print(f"~~~~~ 1 new current span: {hub.scope.span}")
             time.sleep(0.02)
 
         async def count_hits(message):
             if message["type"] == "http.response.start":
                 # this is the code that is call in the "response" phase of the middleware
                 hub = sentry_sdk.Hub.current
+                print(f"~~~ 2 current span: {hub.scope.span}")        
                 with hub.start_span(op="cache.get_item", description="in the response phase of CounterMiddleware") as middleware_span:
-                    print('so something here with response data')
+                    print(f"~~~~~ 2 started span: {middleware_span}")
+                    print(f"~~~~~ 2 hub: {hub}")
+                    print(f"~~~~~ 2 scope: {hub.scope}")
+                    print(f"~~~~~ 2 new current span: {hub.scope.span}")
                     time.sleep(0.04)
 
             await send(message)
